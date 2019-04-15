aa30ad9038aff505518a2f855bae634fc4891e18
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2019-04-15 16:19:25 -0300

    fixed bug

--- pagination.php
+++ pagination.php
@@ -14,10 +14,10 @@ function get_pagination($current_page, $pages_count, $maxLinks = 1) {
         <div class="styled-pagination">
             <ul class="clearfix">
             <?php
-            echo '<li><a href="'.get_the_permalink(get_the_ID()).'"?pg=1" aria-label="Previous"><span>&laquo;</span></a></li>';
+            echo '<li class="prev"><a href="'.get_the_permalink(get_the_ID()).'"?pg=1" aria-label="Previous"><span>&laquo;</span></a></li>';
             for($i = $current_page - $maxLinks; $i <= $current_page - 1; $i++):
                 if($i >= 1):
-                    echo '<li><a href="'.get_the_permalink(get_the_ID()).'?pg=$i">$i</a></li>';
+                    echo '<li><a href="'.get_the_permalink(get_the_ID()).'?pg='.$i.'">'.$i.'</a></li>';
                 endif;
             endfor;
 
@@ -25,12 +25,12 @@ function get_pagination($current_page, $pages_count, $maxLinks = 1) {
 
             for($i = $current_page + 1; $i <= $current_page + $maxLinks; $i++):
                 if($i <= $pages_count):
-                    echo '<li><a href="'.get_the_permalink(get_the_ID()).'?pg=$i">$i</a></li>';
+                    echo '<li><a href="'.get_the_permalink(get_the_ID()).'?pg='.$i.'">'.$i.'</a></li>';
                 endif;
             endfor;
-            echo '<li><a class="page-link" href="'.get_the_permalink(get_the_ID()).'?pg='.$pages_count.'" aria-label="Next"><span>&raquo;</span></a></li>';
+            echo '<li class="next"><a class="page-link" href="'.get_the_permalink(get_the_ID()).'?pg='.$pages_count.'" aria-label="Next"><span>&raquo;</span></a></li>';
             ?>
             </ul>
         </div>
     <?php endif;
-} ?>
+}
