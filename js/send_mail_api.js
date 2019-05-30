f0d65bd025b8f888ba2480b629bc1a326536e8e3
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2019-05-30 10:34:48 -0300

    update

--- js/send_mail_api.js
+++ js/send_mail_api.js
@@ -6,85 +6,94 @@ $('#contact_form').on('submit', function(){
     var phone       = $('#form_phone').val();
     var subject     = $('#form_subject').val();
     var message     = $('#form_message').val();
+
     // Validate values
     if( name.trim() == '' ){
-	iziToast.warning({
-	    title: 'Atenção!',
-	    message: 'Insira o seu nome.',
-	    position: 'topRight'
-
-	});
+		iziToast.warning({
+		    title: 'Oops! ',
+		    message: 'O campo <b>nome</b> é obrigatório.',
+		    position: 'topRight'
+		});
 
     }else if( email.trim() == '' ){
-	iziToast.warning({
-	    title: 'Atenção!',
-	    message: 'Insira seu e-mail.',
-	    position: 'topRight'
-	});
+        iziToast.warning({
+		    title: 'Oops! ',
+		    message: 'O campo <b>e-mail</b> é obrigatório.',
+		    position: 'topRight'
+		});
 
     }else if( phone.trim() == '' ){
-	iziToast.warning({
-	    title: 'Atenção!',
-	    message: 'Insira seu telefone.',
-	    position: 'topRight'
-	});
+        iziToast.warning({
+		    title: 'Oops! ',
+		    message: 'O campo <b>telefone</b> é obrigatório.',
+		    position: 'topRight'
+		});
 
     }else if( subject.trim() == '' ){
-	iziToast.warning({
-	    title: 'Atenção!',
-	    message: 'Insira o assunto de envio.',
-	    position: 'topRight'
-	});
+        iziToast.warning({
+		    title: 'Oops! ',
+		    message: 'O campo <b>assunto</b> é obrigatório.',
+		    position: 'topRight'
+		});
 
     }else if( message.trim() == '' ){
-	iziToast.warning({
-	    title: 'Atenção!',
-	    message: 'Escreva sua mensagem.',
-	    position: 'topRight'
-	});
+        iziToast.warning({
+		    title: 'Oops! ',
+		    message: 'O campo <b>mensagem</b> é obrigatório.',
+		    position: 'topRight'
+		});
 
     }else{
-	$('#contact_form button').html('Enviando...');
-	let content = `
-		<h2 style="color:#002a4e">Formulário de Contato</h2>
-		<p>
-			<b>Nome:</b> ${name} <br>
-			<b>E-mail:</b> ${email} <br>
-			<b>Telefone:</b> ${phone} <br>
-			<b>Assunto:</b> ${subject} <br>
-		</p>
-		<p>
-			<b>Mensagem:</b><br> ${message}
-		</p>
-	`;
+        $('#contact_form button').html('Enviando...');
 
-	$.ajax({
+        var url = window.location.href; // Returns full URL (https://example.com/path/example.php)
+    	let content = `
+    		<div style="max-width:550px;border:1px solid #dedede;border-radius:22px;margin:0 auto;padding:30px 35px;display:flex;align-items:center;justify-content:center;">
+				<div>
+					<h2 style="color:#2196f3;font-size:25px;margin-bottom:20px;">
+						Formulário de Contato - Nome site
+					</h2>
+					<p>
+						<b style="font-size:16px">Nome:</b> ${name} <br>
+						<b style="font-size:16px">E-mail:</b> ${email} <br>
+						<b style="font-size:16px">Telefone:</b> ${phone} <br>
+						<b style="font-size:16px">Assunto:</b> ${subject} <br>
+					</p>
+					<p>
+						<b style="margin-bottom:20px;font-size:16px">Mensagem:</b><br> ${message}
+					</p><hr>
+					<small style="font-size:12px;color:#595959;">E-mail enviado através da página do site <b><a href="${url}" target="_blank">${url}</a><b>.</small>
+				</div>
+			</div>
+    	`;
+
+        $.ajax({
 		    url: 'https://mailsender.dvulgsolucoes.com.br/api/email/send',
 		    method: 'POST',
-		    data: { request:'Local', from:email, to:'exemplo@gmail.com', subject:subject, message:content },
+		    data: { request:'Local', from:email, to:'exemple@gmail.com', subject:subject, message:content },
 		    success: function(response){
-			let data = JSON.parse(response);
+		        let data = JSON.parse(response);
 
-			if( data.success ) {
-				$('#form_name').val("");
-				$('#form_email').val("");
-				$('#form_phone').val("");
-				$('#form_subject').val("");
-				$('#form_message').val("");
+		    	if( data.success ) {
+		    		$('#form_name').val("");
+					$('#form_email').val("");
+					$('#form_phone').val("");
+					$('#form_subject').val("");
+					$('#form_message').val("");
+                    $('#contact_form button').html('Enviar <i class="icon-next"></i>');
 
-		    $('#contact_form button').html('Enviar');
-				iziToast.success({
-				    title: 'Mensagem enviada com sucesso! ',
-				    position: 'topRight',
-				    timeout: 10500
-				});
+			    	iziToast.success({
+					    title: 'Mensagem enviada com sucesso!',
+					    position: 'topRight',
+					    timeout: 10500
+					});
 			    }else {
-		    $('#contact_form button').html('Enviar Novamente');
-				iziToast.danger({
-				    title: 'Ops! Ocorreu um erro ao tentar enviar sua mensagem. ',
-				    position: 'topRight',
-				    timeout: 10500
-				});
+                    $('#contact-form button').html('Enviar Novamente');
+                    iziToast.danger({
+					    title: 'Oops! Ocorreu um erro ao enviar sua mensagem. Tente novamente mais tarde.',
+					    position: 'topRight',
+					    timeout: 10500
+					});
 			    }
 		    }
 		});
