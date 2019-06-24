d3e714035296727331d106000c7c1cd6db58f5a0
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2019-06-24 15:44:52 -0300

    Update pagination.php

--- pagination.php
+++ pagination.php
@@ -1,24 +1,16 @@
 <?php
-// USE IN POST TYPE LIST
-
 /*----------  PAGINATION  ----------*/
-/*
-$posts_total    = new WP_Query(array('post_type' => 'clipping', 'posts_per_page' => -1));
+$posts_total    = new WP_Query(array('post_type' => 'post', 'posts_per_page' => -1));
 $posts_count    = $posts_total->post_count;//total de posts
 $posts_per_page = 6;
 $pages_count    = ceil($posts_count / $posts_per_page);
 $current_page   = ( isset($_GET['pg']) && $_GET['pg'] > 1 && $_GET['pg'] <= $pages_count ) ? $_GET['pg'] : 1;
-*/
-
 /*----------  Loop  ----------*/
-/* 
-$sql_clipping = new WP_Query(array('post_type' => 'clipping', 'posts_per_page' => $posts_per_page, 'order' => 'DESC', 'paged' => $current_page));
-
-if( $sql_clipping->have_posts() ): while( $sql_clipping->have_posts() ): $sql_clipping->the_post(); ?>
+$sql_posts = new WP_Query(array('post_type' => 'post', 'posts_per_page' => $posts_per_page, 'order' => 'DESC', 'paged' => $current_page));
+if( $sql_posts->have_posts() ): while( $sql_posts->have_posts() ): $sql_posts->the_post(); ?>
 
 
 <?php endwhile; endif; wp_reset_query(); ?>
-*/
 
 // FUNCTION
 
@@ -30,7 +22,7 @@ if( $sql_clipping->have_posts() ): while( $sql_clipping->have_posts() ): $sql_cl
  *
  * $current_page - Página atual
  * $pages_count - total de páginas de posts
-  * $maxLinks - tamanho da paginação (A esqueda e a direita) Default = 2
+ * $maxLinks - tamanho da paginação (A esqueda e a direita) Default = 2
  */
 function get_pagination($current_page, $pages_count, $maxLinks = 2) {
 	//Limite de link antes e depois
