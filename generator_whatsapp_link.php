678b6c931176f395e20346dbfc6bcd09b770b115
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2019-09-02 15:11:10 -0300

    Update generator_whatsapp_link.php

--- generator_whatsapp_link.php
+++ generator_whatsapp_link.php
@@ -6,7 +6,7 @@
 function send_whatsapp($number, $prefix = '55') {
     $number = $prefix . $number;
     $number = preg_replace('/\(|\)|\s+|\+|\-/', '', $number);
-    $msg = 'Olá, preciso da sua ajuda, Fácil Fibra!';
+    $msg = 'Olá, coloque sua frase aqui!';
     $msg = str_replace(' ', '%20', $msg); //mensage
 
     $url = 'https://api.whatsapp.com/send?phone=' . $number . '&text=' . $msg;
