a48464d6e6f9c738bbdc5213652aaf325f64fab3
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2019-11-21 09:01:08 -0300

    Create calling-functions.php

---
+++ wc/calling-functions.php
@@ -0,0 +1,16 @@
+<?php
+
+// get shop
+get_permalink( woocommerce_get_page_id( 'shop' ) )
+
+// get cart content and count
+WC()->cart->get_cart_contents_count()
+
+// get cart total value
+WC()->cart->total
+
+// get cart url
+wc_get_cart_url()
+
+// check if the user is logged in
+is_user_logged_in()
