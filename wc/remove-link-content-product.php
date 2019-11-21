518c5479c644a971953b680bc9bcce3434a59afb
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2019-11-21 08:51:42 -0300

    Create remove-link-content-product.php

---
+++ wc/remove-link-content-product.php
@@ -0,0 +1,4 @@
+<?php
+
+remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5);
