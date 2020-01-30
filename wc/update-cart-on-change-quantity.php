731e6383d73cfffac07464c132a7cde05969f9c4
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2020-01-30 09:43:05 -0300

    Create update-cart-on-change-quantity.php

---
+++ wc/update-cart-on-change-quantity.php
@@ -0,0 +1,14 @@
+<?php
+// Atualiza o carrinho ao mudar a quantidade
+add_action( 'wp_footer', 'programa_wp_atualizar_qtd_carrinho' ); 
+function programa_wp_atualizar_qtd_carrinho() { 
+	if( is_cart() ) { 
+		?> 
+		<script type="text/javascript"> 
+			jQuery('div.woocommerce').on('click', 'input.qty', function(){ 
+			jQuery("[name='update_cart']").trigger("click"); 
+			}); 
+		</script> 
+		<?php 
+	} 
+}
