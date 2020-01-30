287e267ff6af507f83657e260034eb266df6a89b
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2020-01-30 09:05:26 -0300

    Update get-products-in-cart.php

--- wc/get-products-in-cart.php
+++ wc/get-products-in-cart.php
@@ -1,17 +1,50 @@
-<?php 
-global $woocommerce;
-$items = $woocommerce->cart->get_cart();
+<ul>
+  <?php do_action( 'woocommerce_before_cart_contents' ); ?>
+  <?php if(WC()->cart->is_empty()): ?>
+    <li class="empty-cart-txt">Seu Carrinho está Vazio!</li>
+  <?php else: ?>
 
-foreach($items as $item => $values) { 
-	$_product =  wc_get_product( $values['data']->get_id() );
-	//product image
-	$getProductDetail = wc_get_product( $values['product_id'] );
-	echo $getProductDetail->get_image(); // accepts 2 arguments ( size, attr )
+    <?php
+    foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
 
-	echo "<b>".$_product->get_title() .'</b>  <br> Quantity: '.$values['quantity'].'<br>'; 
-	$price = get_post_meta($values['product_id'] , '_price', true);
-	echo "  Price: ".$price."<br>";
-	/*Regular Price and Sale Price*/
-	echo "Regular Price: ".get_post_meta($values['product_id'] , '_regular_price', true)."<br>";
-	echo "Sale Price: ".get_post_meta($values['product_id'] , '_sale_price', true)."<br>";
-}
+      $_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+      $product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
+
+      if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+
+        $product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );				
+        $thumbnail         = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+        $product_name      = apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_title() ), $cart_item, $cart_item_key );
+        $product_price     = apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key );
+        $product_subtotal  = apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key );
+        $attributes        = '';
+        //Variation
+        $attributes .= $_product->is_type('variable') || $_product->is_type('variation') ? wc_get_formatted_variation($_product) : '';
+        // Meta data
+        if(version_compare( WC()->version , '3.3.0' , "<" )){
+          $attributes .=  WC()->cart->get_item_data( $cart_item );
+        }
+        else{
+          $attributes .=  wc_get_formatted_cart_item_data( $cart_item );
+        }
+    ?>
+        <li class="item-product" data-item_key="<?php echo $cart_item_key; ?>">
+          <div class="img-product">
+            <a href="<?php echo $product_permalink; ?>"><?php echo $thumbnail; ?></a>
+          </div>
+          <div class="info-product">
+            <div class="name"><?php echo $product_name; ?></div><!-- /.name -->
+
+            <?php echo $attributes ? $attributes : ''; ?> 
+
+            <div class="price">
+              <span class="qnt"><?php echo $cart_item['quantity']; ?></span> X <span><?php echo $product_price; ?></span> 
+            </div><!-- /.price -->
+          </div><!-- /.info-product -->
+
+          <span class="delete remove-item" data-cart-item-key="<?=$cart_item_key;?>">x</span>
+        </li>
+      <?php } ?>
+    <?php } ?>
+  <?php endif; ?>
+</ul>
