bf113ca436f98d8cbdeceb8410c3ff5fb466df3d
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2020-01-30 09:40:22 -0300

    Update remove_item_cart.js

--- wc/js/remove_item_cart.js
+++ wc/js/remove_item_cart.js
@@ -1,58 +1,58 @@
-// remove item in cart
-$('BUTTON_TO_REMOVE_HERE').click(function (e) {
-  e.preventDefault();
-  let ajaxurl = $('meta[name=urlajax]').attr('content'); // create meta with url site
-  let parentItem = $(this).parent(); // get wrap element
-  let parentItem2 = $(this).parent().parent(); // get the wrap parent Ex: <ul>
+	// remove item in cart
+	$('.remove-item').click(function (e) {
+		e.preventDefault();
+		let ajaxurl = $('meta[name=urlajax]').attr('content');
+		let parentItem = $(this).parent();
+		let parentItem2 = $(this).parent().parent();
 
-  $(parentItem).addClass('loading');
+		$(parentItem).addClass('loading');
 
-  $.ajax({
-    type: "POST",
-    url: ajaxurl + '/wp-admin/admin-ajax.php',
-    data: {
-      action: 'remove_item_from_cart',
-      'cart_item_key': String($(this).data('cart-item-key'))
-    },
-    success: function (res) {
-      if (res) {
-        let countItems = parseInt($('.icon-cart span').html()); //qnt total
-        let qnt = parentItem.find('.price .qnt').html(); // qnt item removed
-        $('.icon-cart span').html(countItems - qnt);
+		$.ajax({
+			type: "POST",
+			url: wc_add_to_cart_params.ajax_url,
+			data: {
+				action: 'remove_item_from_cart',
+				'cart_item_key': String($(this).data('cart-item-key'))
+			},
+			success: function (res) {
+				if (res) {
+					let countItems = parseInt($('.icon-cart span').html()); //qnt total
+					let qnt = parentItem.find('.price .qnt').html(); // qnt item removed
+					$('.icon-cart span').html(countItems - qnt);
 
-        // remove item
-        $('.dropdown-box ul li[data-id=' + parentItem.attr('data-id') + ']').remove();
+					// remove item
+					$('.dropdown-box ul li[data-id=' + parentItem.attr('data-id') + ']').remove();
 
-        // if empty, show message
-        if (parentItem2.has('li').length == 0) {
-          $('.dropdown-box ul').html('<li class="empty-cart-txt">Seu Carrinho está Vazio!</li>');
-          $('.btn-cart').html(`<a href="${ajaxurl}/loja" class="view-cart">Fazer compras</a>`);
-          $('.total .price, .price-cart').html('R$ 00,00');
-        }
+					// if empty, show message
+					if (parentItem2.has('li').length == 0) {
+						$('.dropdown-box ul').html('<li class="empty-cart-txt">Seu Carrinho está Vazio!</li>');
+						$('.btn-cart').html(`<a href="${ajaxurl}/loja" class="view-cart">Fazer compras</a>`);
+						$('.total .price, .price-cart').html('R$ 00,00');
+					}
 
-        Swal.fire({
-          type: 'success',
-          title: 'Item removido do carrinho'
-        });
+					Swal.fire({
+						type: 'success',
+						title: 'Item removido do carrinho'
+					});
 
-        // reload cart page to update data
-        if (document.location.href == ajaxurl + '/carrinho/') {
-          document.location.reload(true);
-        }
-      }
-    }
-  });
-});
+					// reload cart page to update data
+					if (document.location.href == ajaxurl + '/carrinho/') {
+						document.location.reload(true);
+					}
+				}
+			},
+			error: function (res) {
+				if (res) {
+					$(parentItem).removeClass('loading');
 
+					Swal.fire({
+						title: 'Ops!',
+						text: 'Algo deu errado, o item não removido. Tente novamente mais tarde.',
+						type: 'error',
+						confirmButtonText: 'Fechar'
+					});
+				}
+			}
 
-// PHP FUNCTION 
-// function remove_item_from_cart() {
-//   $cart_item_key = $_POST['cart_item_key'];
-//   if($cart_item_key){
-//      WC()->cart->remove_cart_item($cart_item_key);
-//      return true;
-//   } 
-//   return false;
-// }
-// add_action('wp_ajax_remove_item_from_cart', 'remove_item_from_cart');
-// add_action('wp_ajax_nopriv_remove_item_from_cart', 'remove_item_from_cart');
+		});
+	});
