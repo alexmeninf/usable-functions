8c2761799e73b458535801441c5ea4ded913517d
Alexandre <alex.menin11@gmail.com>
2020-05-18 17:57:50 -0300

    feat: update menu

--- components/drawerMenu/script.js
+++ components/drawerMenu/script.js
@@ -1,34 +1,65 @@
 drawerMobile = () => {
-  let drawer = '.drawer-mobile',
-    drawerButton = '.drawerButton',
-    drawerClose = '.close-menu',
-    listMenu = '.drawer-mobile ul';
+    let drawer = '.drawer-mobile',
+      drawerButton = '.drawerButton',
+      drawerClose = '.close-menu',
+      listMenu = '.drawer-mobile ul',
+      swipeTouch = {};
 
-  // Open
-  $(drawerButton).click(function (e) {
-    e.preventDefault();
+    // Open
+    $(drawerButton).click(function (e) {
+      e.preventDefault();
 
-    $('body').css('overflow', 'hidden');
-    $(drawer).removeClass('drawerClosed');
-    $(drawer).addClass('drawerOpen');
-    $(listMenu).animate({
-      left: '0'
+      $('body').css('overflow', 'hidden');
+      $(drawer).removeClass('drawerClosed');
+      $(drawer).addClass('drawerOpen');
+      $(listMenu).animate({
+        left: '0'
+      });
+      setTimeout(() => {
+        $(drawerClose).addClass('open');
+      }, 200);
     });
-    setTimeout(() => {
-      $(drawerClose).addClass('open');
-    }, 200);
-  });
 
-  // close
-  $('.drawer-mobile ul li a, .close-menu').click(function () {
-    $('body').css('overflow-y', 'scroll');
-    $(drawerClose).removeClass('open');
-    $(listMenu).animate({
-      left: '-100%'
+    // close
+    $('.drawer-mobile ul li a, .close-menu').click(function () {
+      $('body').css('overflow-y', 'scroll');
+      $(drawerClose).removeClass('open');
+      $(listMenu).animate({
+        left: '-100%'
+      });
+      setTimeout(() => {
+        $(drawer).addClass('drawerClosed');
+        $(drawer).removeClass('drawerOpen');
+      }, 600);
     });
-    setTimeout(() => {
-      $(drawer).addClass('drawerClosed');
-      $(drawer).removeClass('drawerOpen');
-    }, 600);
-  });
-}
+
+    if (!navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
+      swipeTouch = document.getElementById('swipe-area');
+    } else {
+      swipeTouch = document;
+    }
+
+    swipeTouch.addEventListener('swiped-right', function (e) {
+      $('body').css('overflow', 'hidden');
+      $(drawer).removeClass('drawerClosed');
+      $(drawer).addClass('drawerOpen');
+      $(listMenu).animate({
+        left: '0'
+      });
+      setTimeout(() => {
+        $(drawerClose).addClass('open');
+      }, 200);
+    });
+
+    document.getElementById('drawer').addEventListener('swiped-left', function (e) {
+      $('body').css('overflow-y', 'scroll');
+      $(drawerClose).removeClass('open');
+      $(listMenu).animate({
+        left: '-100%'
+      });
+      setTimeout(() => {
+        $(drawer).addClass('drawerClosed');
+        $(drawer).removeClass('drawerOpen');
+      }, 600);
+    });
+  }
