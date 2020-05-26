c85d8668832f15f122a02b9256dbee0ec66857f9
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2020-05-26 18:10:40 -0300

    Update functions.php

--- wp/load-more-posts/functions.php
+++ wp/load-more-posts/functions.php
@@ -1,11 +1,11 @@
-<?php
-
 function load_more_posts() {
 	$next_page = $_POST['current_page'] + 1;
 	
   $query = new WP_Query([
-		'post_type'      => 'post',
-    'posts_per_page' => 10,
+		'post_type' => 'medico',
+		'order' => 'ASC',
+		'offset' => 3, // offset dos posts no index.php
+		'posts_per_page' => 3, //mesmo do index.php
     'paged'          => $next_page
   ]);
   if ($query->have_posts()) :
@@ -13,14 +13,14 @@ function load_more_posts() {
 
     while ($query->have_posts()) : $query->the_post();
 
-      get_template_part('inc/item-post');
+			get_template_part('template-parts/cards/content-doctor');
 
     endwhile;
 
     wp_send_json_success(ob_get_clean());
     
   else :
-    wp_send_json_error('Nenhum post foi encontrado');
+    wp_send_json_error('Nenhum médico post foi encontrado');
 
   endif;
 }
