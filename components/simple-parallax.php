ee29fdd24aacd698c25650f9235231c17947e0ed
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2020-05-27 14:26:02 -0300

    Create simple-parallax.php

---
+++ components/simple-parallax.php
@@ -0,0 +1,68 @@
+<div class="parallax-box">
+		<div class="parallax-image" style="background-image: url('http://via.placeholder.com/1920x600')"></div>
+
+      <div class="post-overlay-inner">
+        <h1 class="post-title"><?php the_title() ?></h1>				
+    </div>
+	</div>
+  
+  
+  <style>
+  .parallax-box {
+    width: 100%;
+    position: relative;
+    height: 460px;
+    overflow: hidden;
+    top: 0;
+  }
+  
+  .parallax-image {
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    position: relative;
+    width: 100%;
+    height: 760px;
+    border-radius: 3px;
+    min-height: 660px;
+    margin-top: -150px;
+    -webkit-transform: translate3d(0,0,0);
+    transform: translate3d(0,0,0);
+  }
+</style>
+  
+  
+  <script>
+  var makeParallax = function () {
+	var scrollTop = $(this).scrollTop();
+	if (scrollTop < 300)
+		$('.parallax-box .parallax-image').css({
+			'transform': 'translate3d(0px, ' + (scrollTop / 2) + 'px, 0px)',
+			'filter': 'blur(' + scrollTop / 10 + 'px)',
+		});
+
+	var opacity = 1;
+	if (scrollTop > 10 && scrollTop < 50)
+		opacity = 0.9;
+	else if (scrollTop >= 50 && scrollTop < 100)
+		opacity = 0.8;
+	else if (scrollTop >= 100 && scrollTop < 150)
+		opacity = 0.7;
+	else if (scrollTop >= 150 && scrollTop < 200)
+		opacity = 0.6;
+	else if (scrollTop >= 200 && scrollTop < 250)
+		opacity = 0.5;
+	else if (scrollTop >= 250 && scrollTop < 300)
+		opacity = 0.4;
+	else if (scrollTop >= 300 && scrollTop < 350)
+		opacity = 0.3;
+	else if (scrollTop >= 350 && scrollTop < 400)
+		opacity = 0.2;
+	else if (scrollTop >= 400)
+		opacity = 0;
+
+	$('.parallax-box .post-overlay-inner').css({
+		'opacity': opacity
+	});
+}
+</script>
