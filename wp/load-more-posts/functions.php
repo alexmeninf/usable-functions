465ab6f0efa4ff45b68d92f703b4bf5fe89ef805
Alexandre Menin <alex.menin11@gmail.com>
2020-06-10 15:09:03 -0300

    Update functions.php

--- wp/load-more-posts/functions.php
+++ wp/load-more-posts/functions.php
@@ -1,28 +1,39 @@
 function load_more_posts() {
+	header("Content-Type: text/html");
+
 	$next_page = $_POST['current_page'] + 1;
-	
+	$max_pages = $_POST['max_pages'];
+	$offset_pages = $_POST['offset_pages'];
+	$per_page = $_POST['per_page'];
+
   $query = new WP_Query([
-		'post_type' => 'post',
-		'order' => 'ASC',
-		'offset' => 3, // offset dos posts no index.php
-		'posts_per_page' => 3, //mesmo do index.php
+		'post_type'      => 'post',
+		'offset'         => $offset_pages + $per_page,
+    'posts_per_page' => $per_page,
     'paged'          => $next_page
   ]);
-  if ($query->have_posts()) :
+	if ($query->have_posts()) :
+
     ob_start();
 
-    while ($query->have_posts()) : $query->the_post();
+		while ($query->have_posts()) : $query->the_post();
 
-			the_title();
+			get_template_part('template-parts/post/cards/item-post');
 
-    endwhile;
+		endwhile;
 
-    wp_send_json_success(ob_get_clean());
-    
+		$result = ob_get_contents();
+		ob_end_clean();
+		
   else :
-    wp_send_json_error('Nenhum médico post foi encontrado');
 
-  endif;
+    wp_send_json_error('<div class="mt-5">Você chegou ao fim. :)</div>');
+
+	endif;
+
+	wp_reset_postdata();
+
+	wp_send_json_success($result);
 }
 add_action('wp_ajax_load_more_posts', 'load_more_posts');
 add_action('wp_ajax_nopriv_load_more_posts', 'load_more_posts');
