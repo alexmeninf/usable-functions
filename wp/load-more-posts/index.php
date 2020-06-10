7de2c14dd23d1cf529e3b5b25aefb3458b1cef62
Alexandre Menin <alex.menin11@gmail.com>
2020-06-10 15:07:23 -0300

    Update index.php

--- wp/load-more-posts/index.php
+++ wp/load-more-posts/index.php
@@ -8,18 +8,21 @@
   <body>
     <?php 
     global $wp_query;
+    $offset = 3;
+    $posts_per_page = 8;
     $posts_total    = new WP_Query(array('post_type' => 'post', 'posts_per_page' => -1));
     $posts_count    = $posts_total->post_count;//total de posts
-    $posts_per_page = 3; // o valor deve ser igual no functions.php
     $pages_count    = ceil($posts_count / $posts_per_page);
     ?>
     <main
       class="posts-list"
       data-page="<?= get_query_var('paged') ? get_query_var('paged') : 1; ?>"
       data-max="<?= $pages_count; ?>"
+      data-offset="<?= $offset ?>"
+      data-per_page="<?= $posts_per_page ?>"
     >
     <?php 
-    $loop = new WP_Query(array('post_type' => 'post', 'order' => 'asc', 'posts_per_page' => $posts_per_page));
+    $loop = new WP_Query(array('post_type' => 'post', 'order' => 'desc', 'offset' => $offset, 'ignore_sticky_posts' => 1,'posts_per_page' => $posts_per_page));
       if ($loop->have_posts()) : ?>
         <?php while ($loop->have_posts()) : $loop->the_post(); ?>
           <?php the_title() ?>
