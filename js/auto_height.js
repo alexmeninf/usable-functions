100f94137b61b855b6d30478b01117c1ed68ef36
Alexandre Menin <alex.menin11@gmail.com>
2020-07-02 10:03:31 -0300

    Update auto_height.js

--- js/auto_height.js
+++ js/auto_height.js
@@ -1,15 +1,18 @@
 /*----------  AUTO HEIGHT  ----------*/
-$(window).on('load resize', function () {
-	var proposalHeight = 0;
-	$('.card').each(function () {
-		if ($(this).innerHeight() > proposalHeight) {
-			proposalHeight = $(this).innerHeight();
+autoHeightElements = () => {
+	$(window).on('load resize', function () {
+		let proposalHeight = 0,
+				selectorItem = 'add your class or id';
+		$(selectorItem).each(function () {
+			if ($(this).innerHeight() > proposalHeight) {
+				proposalHeight = $(this).innerHeight();
+			}
+		});
+		// Set height
+		if ($(document).innerWidth() >= BREAKPOINT_HERE) {
+			$(selectorItem).css('height', proposalHeight + 'px');
+		} else {
+			$(selectorItem).css('height', 'auto');
 		}
-	});
-	// Set height
-	if ($(document).innerWidth() >= 500) {
-		$('.card').css('height', proposalHeight + 'px');
-	} else {
-		$('.card').css('height', 'auto');
-	}
-}).trigger('resize');
+	}).trigger('resize');
+}
