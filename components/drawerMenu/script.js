fceec4d8f4a441ed2d4d5c73d1a3ff0b9cd5f49c
Alexandre Menin <alex.menin11@gmail.com>
2020-07-28 17:35:34 -0300

    Update script.js

--- components/drawerMenu/script.js
+++ components/drawerMenu/script.js
@@ -1,17 +1,14 @@
-drawerMobile = () => {
+  drawerMobile = () => {
     let drawer = '.drawer-mobile',
       drawerButton = '.drawerButton',
       drawerClose = '.close-menu',
-      listMenu = '.drawer-mobile ul',
-      submenu = '.submenu',
       drawerClosed = 'drawerClosed',
       drawerOpen = 'drawerOpen',
-      swipeTouch = {};
+      listMenu = '.drawer-mobile ul',
+      submenu = '.submenu',
+      iconAnimate = 'rotateIcon';
 
-    // Open
-    $(drawerButton).click(function (e) {
-      e.preventDefault();
-
+    function openMenuSwiped() {
       $('body').css('overflow', 'hidden');
       $(drawer).removeClass(drawerClosed);
       $(drawer).addClass(drawerOpen);
@@ -21,56 +18,9 @@ drawerMobile = () => {
       setTimeout(() => {
         $(drawerClose).addClass('open');
       }, 200);
-    });
-
-    // close and open submenu
-    $(listMenu + '> li > a,' + drawerClose).click(function () {      
-     let parentLi = $(this).parent('li');
-     let hasSubmenu = $(this).parents('li');
-
-      if (parentLi.find(submenu).length) {
-        hasSubmenu.siblings().find(submenu).slideUp();
-        parentLi.find('>' + submenu).slideToggle();
-
-        hasSubmenu.siblings().find('> a > i').removeClass('rotateIcon');
-        $(this).find('i').toggleClass('rotateIcon');
-        
-        return false;
-
-      } else {
-        $(listMenu + '> li').find(submenu).slideUp();
-        $(listMenu + '> li').find('i').removeClass('rotateIcon');
-        $('body').css('overflow-y', 'scroll');
-        $(drawerClose).removeClass('open');
-        $(listMenu).animate({
-          left: '-100%'
-        });
-        setTimeout(() => {
-          $(drawer).addClass(drawerClosed);
-          $(drawer).removeClass(drawerOpen);
-        }, 600);
-      }
-    });
-
-    if (!navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
-      swipeTouch = document.getElementById('swipe-area');
-    } else {
-      swipeTouch = document;
     }
 
-    swipeTouch.addEventListener('swiped-right', function (e) {
-      $('body').css('overflow', 'hidden');
-      $(drawer).removeClass(drawerClosed);
-      $(drawer).addClass(drawerOpen);
-      $(listMenu).animate({
-        left: '0'
-      });
-      setTimeout(() => {
-        $(drawerClose).addClass('open');
-      }, 200);
-    });
-
-    document.getElementById('drawer').addEventListener('swiped-left', function (e) {
+    function closeMenuSwiped() {
       $('body').css('overflow-y', 'scroll');
       $(drawerClose).removeClass('open');
       $(listMenu).animate({
@@ -80,5 +30,38 @@ drawerMobile = () => {
         $(drawer).addClass(drawerClosed);
         $(drawer).removeClass(drawerOpen);
       }, 600);
+    }
+
+    // Open
+    $(drawerButton).click(function (e) {
+      e.preventDefault();
+      openMenuSwiped();
     });
- }
+
+    // close and open submenu
+    $(listMenu + '> li > a,' + drawerClose).click(function () {
+      let parentLi = $(this).parent('li');
+      let hasSubmenu = $(this).parents('li');
+
+      if (parentLi.find(submenu).length) {
+        hasSubmenu.siblings().find(submenu).slideUp();
+        parentLi.find('>' + submenu).slideToggle();
+        hasSubmenu.siblings().find('> a > i').removeClass(iconAnimate);
+        $(this).find('i').toggleClass(iconAnimate);
+        return false;
+
+      } else {
+        $(listMenu + '> li').find(submenu).slideUp();
+        $(listMenu + '> li').find('i').removeClass(iconAnimate);
+        closeMenuSwiped();
+      }
+    });
+
+    document.addEventListener('swiped-right', function (e) {
+      openMenuSwiped();
+    });
+
+    document.getElementById('drawer').addEventListener('swiped-left', function (e) {
+      closeMenuSwiped();
+    });
+  }
