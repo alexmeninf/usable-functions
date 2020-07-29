ee311ac233cbe9496a24c4f8cdca7eb9bc047796
Alexandre Menin <alex.menin11@gmail.com>
2020-07-29 11:35:09 -0300

    Feat: Update style to iNova NY

--- wp/logo_login_wordpress.php
+++ wp/logo_login_wordpress.php
@@ -3,34 +3,9 @@
 /*===================================================
 =            UPDATE WORDPRESS LOGIN LOGO DARK            =
 ===================================================*/
-function my_login_logo() { ?>
-	<style type="text/css">
-	#login h1 a, .login h1 a {background-image:url('http://s3.dvulgsolucoes.com.br/assets/img/logo-dvulg-wordpress-white.png');background-repeat:no-repeat;height:105px;padding-bottom:30px;width:105px;}body{background:#222!important}.login #backtoblog a,.login #nav a{color:#adadad!important}.login #login_error,.login .message,.login .success,.login form{border-radius:10px}.wp-core-ui .button-primary{background:#eb6358!important;border-color:#eb6358!important;box-shadow:none!important;color:#fff!important;text-decoration:none!important;text-shadow:none!important}
-	</style>
-<?php } add_action( 'login_enqueue_scripts', 'my_login_logo' );
-
-
-
-/*===================================================
-=            UPDATE WORDPRESS LOGIN LOGO Light            =
-===================================================*/
-function my_login_logo() { ?>
-	<style type="text/css">
-	    #login h1 a, .login h1 a {
-	        background-image: url('http://s3.dvulgsolucoes.com.br/assets/img/logo-dvulg-wordpress.png');
-	        background-repeat: no-repeat;
-	        height: 105px;
-	        padding-bottom: 30px;
-	        width: 105px;
-	    }
-
-	    .wp-core-ui .button-primary {
-			background: #eb6358 !important;
-		    border-color: #eb6358 !important;
-		    box-shadow: none !important;
-		    color: #fff !important;
-		    text-decoration: none !important;
-		    text-shadow: none !important;
-	    }
-	</style>
-<?php } add_action( 'login_enqueue_scripts', 'my_login_logo' );
+function wp_custom_logo_in_login() { ?>
+<style type="text/css">
+#login h1 a, .login h1 a {background-image:url('https://comet-space.nyc3.digitaloceanspaces.com/assets/default/logo-inova-circle-pink.png');background-repeat:no-repeat;background-size: 120px;height:120px;width:120px;}body{background:#141414!important}.login #backtoblog a,.login #nav a{color:#adadad!important}.login #login_error,.login .message,.login .success,.login form{border-radius:10px}.wp-core-ui .button-primary{background:#000!important;border-color:#000!important;box-shadow:none!important;color:#fff!important;text-decoration:none!important;text-shadow:none!important;border-radius:0!important;}input[type=text]:focus, input[type=password]:focus {  border-color: #ff0083 !important;
+  box-shadow: none !important;}
+</style>
+<?php } add_action( 'login_enqueue_scripts', 'wp_custom_logo_in_login' );
