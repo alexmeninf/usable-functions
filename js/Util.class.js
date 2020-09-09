bf0af9e3a1d48c8fc180f0c49a0a2e29e756da2b
Alexandre Menin <alex.menin11@gmail.com>
2020-09-09 13:50:44 -0300

    Update Util.class.js

--- js/Util.class.js
+++ js/Util.class.js
@@ -3,9 +3,9 @@ class Util {
   /**
    * Checks if it is an iOS device
    */
-  static iOSDevice() {
-    const isIOS    = /Mac|iPad|iPhone|iPod/.test(navigator.userAgent);
-    const isMacOS  = navigator.platform.indexOf('Mac') != -1;
+  static is_iOS = () => {
+    const isIOS = /Mac|iPad|iPhone|iPod/.test(navigator.userAgent);
+    const isMacOS = navigator.platform.indexOf('Mac') != -1;
     const isSafari = navigator.platform.indexOf('Safari') != -1;
 
     if (isIOS || isMacOS || isSafari) {
@@ -13,5 +13,28 @@ class Util {
     }
 
     return false;
+  }
+
+  /**
+   * 
+   * @param selectorItem Class or ID selector
+   * @param breakpoint  Resolution at which the breakpoint happens 
+   */
+  static autoHeight = (selectorItem, breakpoint) => {
+    $(window).on('load resize', function () {
+      let proposalHeight = 0;
+
+      $(selectorItem).each(function () {
+        if ($(this).innerHeight() > proposalHeight) {
+          proposalHeight = $(this).innerHeight();
+        }
+      });
+
+      if ($(document).innerWidth() >= breakpoint) {
+        $(selectorItem).css('height', proposalHeight + 'px');
+      } else {
+        $(selectorItem).css('height', 'auto');
+      }
+    }).trigger('resize');
   }
 }
