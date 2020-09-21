44c63706021d5096714246360ccd3cc1c5e680ee
Alexandre Menin <alex.menin11@gmail.com>
2020-09-21 08:43:07 -0300

    Update input.php

--- components/form-input/input.php
+++ components/form-input/input.php
@@ -7,19 +7,21 @@
  * @return void
  */
 function input($name, $id, $type, $value = '') { 
+	$value = (trim($value) != '') ? ' value="' .$value. '"' : '';
+
 	if ($type == 'hidden') : ?>
-		<input type="<?= $type ?>" id="<?= $id ?>" name="<?= $id ?>" value="<?= $value ?>">
+		<input type="<?= $type ?>" id="<?= $id ?>" name="<?= $id ?>"<?= $value ?>>
 
 	<?php elseif ($type == 'textarea') : ?>
 		<label class="form-group">
-			<textarea id="<?= $id ?>" name="<?= $id ?>" value="<?= $value ?>" placeholder="&nbsp;"></textarea>
+			<textarea id="<?= $id ?>" name="<?= $id ?>"<?= $value ?> placeholder="&nbsp;"></textarea>
 			<span class="txt"><?= $name ?> <sup>*</sup></span>
 			<span class="bar"></span>
 		</label>
 
 	<?php else: ?>
 		<label class="form-group">
-			<input type="<?= $type ?>" id="<?= $id ?>" value="<?= $value ?>" name="<?= $id ?>" placeholder="&nbsp;">
+			<input type="<?= $type ?>" id="<?= $id ?>" name="<?= $id ?>"<?= $value ?> placeholder="&nbsp;">
 			<span class="txt"><?= $name ?> <sup>*</sup></span>
 			<span class="bar"></span>
 		</label>
