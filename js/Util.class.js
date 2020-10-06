5e14b6f26c79a6af24e94072bfaf718f71723e33
Alexandre Menin <alex.menin11@gmail.com>
2020-10-06 09:03:17 -0300

    Update Util.class.js

--- js/Util.class.js
+++ js/Util.class.js
@@ -3,7 +3,7 @@ class Util {
   /**
    * Checks if it is an iOS device
    */
-  static is_iOS = () => {
+  static is_apple = () => {
     const isIOS = /Mac|iPad|iPhone|iPod/.test(navigator.userAgent);
     const isMacOS = navigator.platform.indexOf('Mac') != -1;
     const isSafari = navigator.platform.indexOf('Safari') != -1;
@@ -12,6 +12,16 @@ class Util {
       return true;
     }
 
+    return false;
+  }
+  
+  /**
+   * Checks if it is an mobile device
+   */
+  function is_mobile() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      return true;
+    }
     return false;
   }
 
