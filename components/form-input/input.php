4e200db38f3ab02f70977978e474ce833a31d227
Alexandre Menin <alex.menin11@gmail.com>
2021-01-20 12:58:02 -0300

    Update input.php

--- components/form-input/input.php
+++ components/form-input/input.php
@@ -1,4 +1,4 @@
-/**
+ /**
  * input
  *
  * @param  mixed $name
@@ -6,22 +6,23 @@
  * @param  mixed $id
  * @return void
  */
-function input($name, $id, $type, $value = '') { 
+function input($name, $id, $type, $is_required = false, $value = '') { 
 	$value = (trim($value) != '') ? ' value="' .$value. '"' : '';
+	$required = $is_required ? 'required' : ''; 
 
 	if ($type == 'hidden') : ?>
 		<input type="<?= $type ?>" id="<?= $id ?>" name="<?= $id ?>"<?= $value ?>>
 
 	<?php elseif ($type == 'textarea') : ?>
 		<label class="form-group">
-			<textarea id="<?= $id ?>" name="<?= $id ?>"<?= $value ?> placeholder="&nbsp;"></textarea>
+			<textarea id="<?= $id ?>" name="<?= $id ?>"<?= $value ?> placeholder="&nbsp;" <?= $required ?>></textarea>
 			<span class="txt"><?= $name ?> <sup>*</sup></span>
 			<span class="bar"></span>
 		</label>
 
 	<?php else: ?>
 		<label class="form-group">
-			<input type="<?= $type ?>" id="<?= $id ?>" name="<?= $id ?>"<?= $value ?> placeholder="&nbsp;">
+			<input type="<?= $type ?>" id="<?= $id ?>" name="<?= $id ?>"<?= $value ?> placeholder="&nbsp;" <?= $required ?>>
 			<span class="txt"><?= $name ?> <sup>*</sup></span>
 			<span class="bar"></span>
 		</label>
