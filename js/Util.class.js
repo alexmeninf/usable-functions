f68d0a02c23986f6028fa6e05fc187393427bf27
Alexandre Menin <alex.menin11@gmail.com>
2021-06-11 14:36:25 -0300

    Update Util.class.js

--- js/Util.class.js
+++ js/Util.class.js
@@ -1,50 +1,60 @@
-class Util {
+/**
+ * Checks if it is an iOS device
+ */
+is_apple = () => {
+  const isIOS = /Mac|iPad|iPhone|iPod/.test(navigator.userAgent);
+  const isMacOS = navigator.platform.indexOf("Mac") != -1;
+  const isSafari = navigator.platform.indexOf("Safari") != -1;
 
-  /**
-   * Checks if it is an iOS device
-   */
-  static is_apple = () => {
-    const isIOS = /Mac|iPad|iPhone|iPod/.test(navigator.userAgent);
-    const isMacOS = navigator.platform.indexOf('Mac') != -1;
-    const isSafari = navigator.platform.indexOf('Safari') != -1;
-
-    if (isIOS || isMacOS || isSafari) {
-      return true;
-    }
-
-    return false;
-  }
-  
-  /**
-   * Checks if it is an mobile device
-   */
-  static is_mobile = () => {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-      return true;
-    }
-    return false;
+  if (isIOS || isMacOS || isSafari) {
+    return true;
   }
 
-  /**
-   * 
-   * @param selectorItem Class or ID selector
-   * @param breakpoint  Resolution at which the breakpoint happens 
-   */
-  static autoHeight = (selectorItem, breakpoint) => {
-    $(window).on('load resize', function () {
-      let proposalHeight = 0;
+  return false;
+};
 
-      $(selectorItem).each(function () {
-        if ($(this).innerHeight() > proposalHeight) {
-          proposalHeight = $(this).innerHeight();
-        }
-      });
+/**
+ * Checks if it is an mobile device
+ */
+is_mobile = () => {
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    return true;
+  }
+  return false;
+};
 
-      if ($(document).innerWidth() >= breakpoint) {
-        $(selectorItem).css('height', proposalHeight + 'px');
-      } else {
-        $(selectorItem).css('height', 'auto');
+/**
+ * @param selectorItem Class or ID selector
+ * @param breakpoint  Resolution at which the breakpoint happens
+ */
+autoHeight = (selectorItem, breakpoint) => {
+  let proposalHeight = 0;
+  let top_of_element = $(selectorItem).offset().top;
+  let bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+
+  if ((bottom_of_screen > top_of_element)) {
+    $(selectorItem).each(function () {
+      if ($(this).innerHeight() > proposalHeight) {
+        proposalHeight = $(this).innerHeight();
       }
-    }).trigger('resize');
+    });
+
+    if ($(document).innerWidth() >= breakpoint) {
+      $(selectorItem).css("min-height", proposalHeight + "px");
+    } else {
+      $(selectorItem).css("min-height", "auto");
+    }
   }
-}
+};
+
+// $(window).on("load", function () {
+// });
+
+
+// $(window).on("scroll", function () {
+// });
+
