8a18ae2d601aee1400260d6db043f892408135e8
Alexandre Menin <alex.menin11@gmail.com>
2021-10-28 10:45:43 -0300

    Update options_page.php

--- wp/options_page.php
+++ wp/options_page.php
@@ -5,8 +5,8 @@
 if( function_exists('acf_add_options_page') ) {
 
 	acf_add_options_page(array(
-		'page_title'  => 'Opções',
-		'menu_title'  => 'Opções',
+		'page_title'  => __('Opções do site', 'menin'),
+		'menu_title'  => __('Opções do site', 'menin'),
 		'menu_slug'   => 'opcoes',
 		'capability'  => 'edit_posts',
 		'redirect'    => false
