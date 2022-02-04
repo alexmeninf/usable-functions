1fc31a0b3a5c38de83b815da27a4db305e8f1640
Alexandre Menin <alex.menin11@gmail.com>
2022-02-04 15:40:15 -0300

    Update create_select_option.php

--- create_select_option.php
+++ create_select_option.php
@@ -1,11 +1,11 @@
 <?php
 
 // Crie cada opção por linha, na seguinte sequencia: valor | Rotulo | atributos
-$subject = '|Selecione uma opção|disabled selected
-valor1|Exemplo 1
-valor2|Exemplo 2
-valor3|Exemplo 3
-valor4|Exemplo 4|disabled';
+$subject = ':Selecione uma opção:disabled selected
+valor1:Exemplo 1
+valor2:Exemplo 2
+valor3:Exemplo 3
+valor4:Exemplo 4:disabled';
   
  // remover mais de dois espaços vazios
 $options = preg_replace('/(\s)+/s', '\\1', $subject);
@@ -23,8 +23,10 @@ echo '<select>';
 foreach ($options as $values) {
 
   // Separar cada campo da opção
-  $args = preg_split('/\|/', $values);
+  $args = preg_split('/\:/', $values);
+  // Atributos
+  $attr = array_key_exists(2, $args) ? $args[2] : '';
   
-  echo '<option value="' . $args[0] . '" ' . $args[2] . '>' . $args[1] . '</option>' . PHP_EOL;
+  echo '<option value="' . trim($args[0]) . '" ' . $attr . '>' . trim($args[1]) . '</option>';
 }
 echo '</select>';
