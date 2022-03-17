cecf8258680056c47f8b6dcecc32c56f9401b67f
Alexandre Menin <alex.menin11@gmail.com>
2022-03-17 10:03:02 -0300

    Update and rename Util.class.js to util.js

--- js/util.js
+++ js/util.js
@@ -60,3 +60,13 @@ autoHeight = (selectorItem, breakpoint) => {
 // $(window).on("scroll", function () {
 // });
 
+// Get paramenters in URL
+getParameterByName = (name, url = window.location.href) => {
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
