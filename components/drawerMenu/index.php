31c42176ab87dcf4ce5ba14391b867dea5ecc7f0
Alexandre Menin <alex.menin11@gmail.com>
2022-03-30 10:52:17 -0300

    Update index.php

--- components/drawerMenu/index.php
+++ components/drawerMenu/index.php
@@ -1,28 +1,28 @@
 <nav class="drawer-mobile" id="drawer">
-	<span class="close-menu" aria-label="Fechar">
-		<i class="fal fa-times"></i>
-	</span>
-	<div class="wrap backdrop-filter">
-		<div class="wrapper-form">
-			<?php get_search_form(); ?>
-			<a class="searchfield_cancel">cancelar</a>
-		</div>
+  <span class="close-menu" aria-label="Fechar">
+    <i class="fal fa-times"></i>
+  </span>
+  <div class="wrap backdrop-filter">
+    <div class="wrapper-form">
+      <?php get_search_form(); ?>
+      <a class="searchfield_cancel">cancelar</a>
+    </div>
 
-		<div class="img-brand">
-			<a href="<?php echo get_home_url() ?>" title="<?php bloginfo('name') ?>">
-				<?php apply_filters('logo_tema', null) ?>
-			</a>
-		</div>
+    <div class="img-brand">
+      <a href="<?php echo get_home_url() ?>" title="<?php bloginfo('name') ?>">
+        <?php apply_filters('logo_tema', null) ?>
+      </a>
+    </div>
 
-		<?php
-		wp_nav_menu(
-			array(
-			'theme_location'  => 'primary',
-			'container'       => 'div',
-			'container_class' => '',
-			'menu_class'      => 'header-menu'
-			)
-		); ?>
-		
-	</div>
+    <?php
+    wp_nav_menu(
+      array(
+        'theme_location'  => 'primary',
+        'container'       => 'div',
+        'container_class' => '',
+        'menu_class'      => 'header-menu'
+      )
+    ); ?>
+
+  </div>
 </nav>
