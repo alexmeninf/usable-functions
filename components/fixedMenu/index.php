bf5b4752ac5cd2b3cc5370f124e1d5d6ee517265
Alexandre Menin <alex.menin11@gmail.com>
2022-03-30 10:53:03 -0300

    Update index.php

--- components/fixedMenu/index.php
+++ components/fixedMenu/index.php
@@ -1,23 +1,25 @@
 <header class="fixed-header expand-xl">
-	<div class="wrap">
-		<div class="img-brand">
-			<a href="<?php echo get_home_url() ?>" title="<?php bloginfo('name') ?>">
-				<?php apply_filters('logo_tema', null) ?>
-			</a>
-		</div>
+  <div class="wrap">
+    <div class="img-brand">
+      <a href="<?php echo get_home_url() ?>" title="<?php bloginfo('name') ?>">
+        <?php apply_filters('logo_tema', null) ?>
+      </a>
+    </div>
 
-		<button type="button" class="btn-menu drawerButton" aria-label="Navigation button"><i class="far fa-bars"></i></button>
+    <div class="d-flex justify-content-between align-items-center">
+      <button type="button" class="btn-menu drawerButton" data-drawer-id="drawer" aria-label="Navigation button"><i class="far fa-bars"></i></button>
 
-		<?php
-		wp_nav_menu(
-			array(
-			'theme_location'  => 'primary',
-			'container'       => 'nav',
-			'container_class' => 'nav-list',
-			'menu_class'      => 'header-menu'
-			)
-		); ?>
+      <?php
+      wp_nav_menu(
+        array(
+          'theme_location'  => 'primary',
+          'container'       => 'nav',
+          'container_class' => 'nav-list',
+          'menu_class'      => 'header-menu'
+        )
+      ); ?>
 
-		<?php get_search_form(); ?>
-	</div>
+      <?php get_search_form(); ?>
+    </div>
+  </div>
 </header>
