f069aeb3fa73bfba6dc12214e018a3c01c76fa40
Alexandre Menin <alex.menin11@gmail.com>
2022-04-05 16:20:04 -0300

    Update single_product-add-button-to-checkout.php

--- wc/single-product/single_product-add-button-to-checkout.php
+++ wc/single-product/single_product-add-button-to-checkout.php
@@ -10,12 +10,11 @@ function add_content_after_addtocart()
   $current_product_id = get_the_ID();
   $product = wc_get_product($current_product_id);
 
-  if (is_user_logged_in() && ($product->is_type('simple') || $product->is_type('variable'))) :
-  ?>
-
-  <div class="d-flex">
-    <button type="button" class="to-checkout-product single_add_to_cart_button button alt <?php $product->is_type('variable') ? 'disabled wc-variation-selection-needed' : '' ?>" style="background-color: #262626;border-color: #262626">Finalizar Pedido</button>
-  </div>
+  if (is_user_logged_in() && WC()->cart->get_cart_contents_count() > 0 ) : ?>
+  
+    <button type="button" 
+    class="to-checkout-product single_add_to_cart_button button <?php $product->is_type('variable') ? 'disabled wc-variation-selection-needed' : '' ?>"
+    ><?php _e('Finalizar Pedido', 'wcstartertheme') ?></button>
 
   <?php
   endif; 
@@ -26,27 +25,19 @@ add_action('woocommerce_after_add_to_cart_button', 'add_content_after_addtocart'
 
 function add_script_single_product() 
 {
-  if ( ! is_product()) return;
-
-  $current_product_id = get_the_ID();
-  $product = wc_get_product($current_product_id);
-  ?>
+  if ( ! is_product()) return; ?>
 
   <script type="text/javascript">
     jQuery(function($) {
       $('button.to-checkout-product').on('click', function(e) {
         e.preventDefault();
 
-        let dataProduct = $('.summary form.cart').serialize();
+        window.location.href = '<?php echo WC()->cart->get_checkout_url() ?>';
 
-        <?php if ($product->is_type('simple')) : ?>
-        window.location.href = '<?php echo WC()->cart->get_checkout_url() ?>?add-to-cart=<?= get_the_ID(); ?>&' + dataProduct;
-        <?php elseif ($product->is_type('variable')) : ?>
-        window.location.href = '<?php echo WC()->cart->get_checkout_url() ?>?' + dataProduct;
-        <?php endif; ?>
       });
     });
   </script>
+
 <?php
 }
 
