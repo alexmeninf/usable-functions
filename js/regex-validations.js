dd5fe13a27fb02cea765e50a09bc861eaac0061d
Alexandre Menin <alex.menin11@gmail.com>
2022-08-12 10:18:53 -0300

    Update regex-validations.js

--- js/regex-validations.js
+++ js/regex-validations.js
@@ -4,3 +4,8 @@ const regexMaiuscula = /[A-Z]/;
 const regexNumeros = /[0-9]/; 
 // Verificar algum caractere especial
 const regexCaracterEspecial = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/; 
+
+
+isMobile = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+isAndroid = () => /Android/i.test(navigator.userAgent);
+isApple = () => /iPhone|iPad|iPod/i.test(navigator.userAgent);
