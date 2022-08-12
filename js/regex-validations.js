1fe636ff8bbcc1804196231549ce0b203a34ef32
Alexandre Menin <alex.menin11@gmail.com>
2022-08-12 10:16:09 -0300

    Create regex-validations.js

---
+++ js/regex-validations.js
@@ -0,0 +1,6 @@
+// Verificar alguma letra maiúscula
+const regexMaiuscula = /[A-Z]/; 
+// Verificar algum número
+const regexNumeros = /[0-9]/; 
+// Verificar algum caractere especial
+const regexCaracterEspecial = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/; 
