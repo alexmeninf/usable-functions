aec6fc937a037b87ef7d039726d1d49d1e61276a
Alexandre Menin <alex.menin11@gmail.com>
2022-08-12 10:18:31 -0300

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,33 +1,4 @@
 /**
- * Checks if it is an iOS device
- */
-is_apple = () => {
-  const isIOS = /Mac|iPad|iPhone|iPod/.test(navigator.userAgent);
-  const isMacOS = navigator.platform.indexOf("Mac") != -1;
-  const isSafari = navigator.platform.indexOf("Safari") != -1;
-
-  if (isIOS || isMacOS || isSafari) {
-    return true;
-  }
-
-  return false;
-};
-
-/**
- * Checks if it is an mobile device
- */
-is_mobile = () => {
-  if (
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    )
-  ) {
-    return true;
-  }
-  return false;
-};
-
-/**
  * @param selectorItem Class or ID selector
  * @param breakpoint  Resolution at which the breakpoint happens
  */
@@ -59,14 +30,3 @@ autoHeight = (selectorItem, breakpoint) => {
 
 // $(window).on("scroll", function () {
 // });
-
-// Get paramenters in URL
-getParameterByName = (name, url = window.location.href) => {
-  name = name.replace(/[\[\]]/g, '\\$&');
-  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
-
