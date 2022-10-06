8de63f48bd91a70f8b02f77d3a3bfde5750d79d7
Alexandre Menin <alex.menin11@gmail.com>
2022-10-06 08:55:07 -0300

    Update index.php

--- components/drawerMenu/index.php
+++ components/drawerMenu/index.php
@@ -26,3 +26,44 @@
 
   </div>
 </nav>
+
+<aside class="drawer-wrapper drawer" id="drawer">
+  <span class="close-menu btn-circle" aria-label="Fechar">
+    <i class="fal fa-times"></i>
+  </span>
+
+  <div class="wrapper">
+    
+    <div class="wrapper-form">
+      <?php get_search_form(); ?>
+      <a class="searchfield_cancel">cancelar</a>
+    </div>
+
+    <div class="menu-brand text-center">
+      <a href="<?= get_home_url() ?>" class="d-inline-block">
+        <?php apply_filters('logo_tema', null); ?>
+      </a>
+    </div>
+
+    <ul class="sidebar-links menu-main list-inline">
+      <li><a href="#">Item</a></li>
+      <li class="menu-item-has-children">
+        <a data-clone="true" data-text="Painel" href="#">Minha conta</a>
+        <ul class="sub-menu">
+          <li>
+            <a href="#">
+              Pedidos
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    
+
+    <div class="sidebar-footer">
+      <button type="button" class="link-sidebar">
+        <span class="clamp-1">Link Footer</span>
+      </button>
+    </div>
+  </div>
+</aside>
